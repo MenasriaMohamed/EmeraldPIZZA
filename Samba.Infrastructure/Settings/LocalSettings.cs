e4234f64ae5ec4a25adc3e30ee5056765d35767d
Menasria Mohamed <35969072+MenasriaMohamed@users.noreply.github.com>
2021-04-23 01:06:18 +0100

    logo

--- Samba.Infrastructure/Settings/LocalSettings.cs
+++ Samba.Infrastructure/Settings/LocalSettings.cs
@@ -217,7 +217,7 @@ html
         public static string CurrencySymbol { get { return CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol; } }
 
         private static int DefaultDbVersion { get { return 24; } }
-        private static string DefaultAppVersion { get { return "Astro-SOFT"; } }
+        private static string DefaultAppVersion { get { return "Astro-Soft"; } }
 
         public static int DbVersion { get { return CanReadVersionFromFile() ? Convert.ToInt32(GetVersionDat("DbVersion")) : DefaultDbVersion; } }
         public static string AppVersion { get { return CanReadVersionFromFile() ? GetVersionDat("AppVersion") : DefaultAppVersion; } }
